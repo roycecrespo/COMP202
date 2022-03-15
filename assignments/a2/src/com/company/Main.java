a3865ef3d6108dfbdb7bd5cc5de30491526569aa
nina <liliamnina@gmail.com>
2022-03-15 17:01:41 -0400

    added description for a2

--- assignments/a2/src/com/company/Main.java
+++ assignments/a2/src/com/company/Main.java
@@ -1,11 +1,40 @@
 package com.company;
 
+/** A2
+ *
+ * description
+ * Write a method alphaString() which takes two Strings as input and returns the String between the two
+ * that comes first in the alphabet. For example, alphaString("banana", "apple") returns the String
+ * "apple", while alphaString("snake", "squirrel") returns the String "snake".
+ *
+ *
+ * examples
+ * 1
+ * input: "banana", "apple"
+ * output: "apple"
+ *
+ * 2
+ * input: "snake", "squirrel"
+ * output: "snake"
+ *
+ * 3
+ * input: "zoo", "zoo"
+ * output: "zoo"
+ *
+ * 4
+ * input: "planet", "planetarium"
+ * output: "planet"
+ *
+ * */
+
+
 public class Main {
 
     public static void main(String[] args) {
         String s1 = "banana";
         String s2 = "apple";
-
+        System.out.println("The input strings are :" + s1 + " and " + s2);
+        System.out.println("The alpha string is: ");
 
         // TODO: call the method alphaString that takes s1 and s2 as parameters and print the string the method returns
     }
