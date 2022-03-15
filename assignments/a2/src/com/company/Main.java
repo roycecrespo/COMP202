0a3dbdd65ffb7f29e74bfbc76bc26c2ab828aed3
nina <liliamnina@gmail.com>
2022-03-15 16:50:09 -0400

    added folder structure and a2

---
+++ assignments/a2/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String s1 = "banana";
+        String s2 = "apple";
+
+
+        // TODO: call the method alphaString that takes s1 and s2 as parameters and print the string the method returns
+    }
+
+    // TODO: write in the comments below how you would find the alpha string in steps
+    /** finding the alpha string
+     * step 1: ...
+     * step 2: ...
+     * ...
+     * */
+
+    // TODO: create the method alphaString that takes 2 strings as parameters and returns the alpha string
+    // TODO: based on the steps defined in the comments, implement the function alphaString
+
+}
